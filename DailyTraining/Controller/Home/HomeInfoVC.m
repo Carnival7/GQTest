cbb0f0e504f49fc8553f74bf8d5e3e4ea0bfa0fd
sunqi <sunqi@dongdao.net>
2017-03-10 10:14:44 +0800

    cause：底部创建textfield，弹出键盘不能遮挡  solution：done

--- DailyTraining/Controller/Home/HomeInfoVC.m
+++ DailyTraining/Controller/Home/HomeInfoVC.m
@@ -7,19 +7,26 @@
 //
 
 #import "HomeInfoVC.h"
+#import "ZYKeyboardUtil.h"
 
-@interface HomeInfoVC ()
+#define MARGIN_KEYBOARD 10
 
+@interface HomeInfoVC ()<UITextFieldDelegate>
+@property (nonatomic, strong) UITextField *bottomText;
+@property (strong, nonatomic) ZYKeyboardUtil *keyboardUtil;
 @end
 
 @implementation HomeInfoVC
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    
+
     [self addView];
-    
+    [self configKeyBoardRespond];
+}
+
+- (void)dealloc {
+    self.bottomText.delegate = nil;
 }
 
 - (void)addView{
@@ -50,9 +57,51 @@
         make.top.equalTo(self.view).offset(90);
         make.height.equalTo(@15);
     }];
+    
+    
+    _bottomText = [[UITextField alloc] init];
+    _bottomText.delegate = self;
+    _bottomText.placeholder = @"请在此输入...";
+    _bottomText.backgroundColor = [UIColor whiteColor];
+    _bottomText.tintColor = [UIColor hexStringToColor:@"#666666"];
+    _bottomText.textColor = [UIColor hexStringToColor:@"#666666"];
+    _bottomText.font = [UIFont systemFontOfSize:15];
+    _bottomText.layer.borderWidth = 1.0f;
+    _bottomText.layer.cornerRadius = 2;
+    _bottomText.layer.borderColor = [UIColor hexStringToColor:@"#cccccc"].CGColor;
+    [self.view addSubview:_bottomText];
+    [_bottomText mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.bottom.equalTo(self.view).offset(-20);
+        make.left.equalTo(self.view).offset(20);
+        make.right.equalTo(self.view).offset(-20);
+        make.height.equalTo(@30);
+    }];
 }
 
+#pragma mark - /************************* 自定义键盘 ***************************/
+- (void)configKeyBoardRespond {
+    self.keyboardUtil = [[ZYKeyboardUtil alloc] initWithKeyboardTopMargin:MARGIN_KEYBOARD];
+    __weak HomeInfoVC *weakSelf = self;
+    
+    // 全自动键盘弹出/收起处理 (需调用keyboardUtil 的 adaptiveViewHandleWithController:adaptiveView:)
+    [_keyboardUtil setAnimateWhenKeyboardAppearAutomaticAnimBlock:^(ZYKeyboardUtil *keyboardUtil) {
+        [keyboardUtil adaptiveViewHandleWithController:weakSelf adaptiveView:weakSelf.bottomText, nil];
+    }];
+    
+    //获取键盘信息
+    [_keyboardUtil setPrintKeyboardInfoBlock:^(ZYKeyboardUtil *keyboardUtil, KeyboardInfo *keyboardInfo) {
+        //NSLog(@"\n\n拿到键盘信息 和 ZYKeyboardUtil对象");
+    }];
+}
 
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    [self.bottomText resignFirstResponder];
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    [self.bottomText resignFirstResponder];
+    return YES;
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
